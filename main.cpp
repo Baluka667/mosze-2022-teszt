91139767d106a6d35a7a495944f7f3c655772ab1
Baluka667 <nagybalazs.a2012@gmail.com>
2022-09-09 19:04:23 +0200

    hibak kijavitasa

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];//NELEMENTS volt irva N_ELEMENTS HELYETT
-    std::cout << '1-100 ertekek duplazasa'// stringet "" koze teszunk es "<<endl;" hianya
-    for (int i = 0;)//" i<N_ELEMENTS; i++" hianya a zarojelbol
+    int *b = new int[N_ELEMENTS];//NELEMENTS volt irva N_ELEMENTS HELYETT
+    std::cout << "1-100 ertekek duplazasa"<<endl;// stringet "" koze teszunk es "<<endl;" hianya
+    for (int i = 0; i<N_ELEMENTS; i++)//" i<N_ELEMENTS; i++" hianya
     {
-        b[i] = i * 2;//mivel 0- indult az elso adat mielott megszorozzuk hozza kell adni 1-et
+        b[i] = (i+1) * 2;//mivel 0- indult az elso adat mielott megszorozzuk hozza kell adni 1-et
     }
-    for (int i = 0; i; i++)// "<N_ELEMENTS" hianya
+    for (int i = 0; i<N_ELEMENTS; i++) // "<N_ELEMENTS" hianya
     {
-        std::cout << "Ertek:"// "<<b[i]<<std::endl;" hianya
+        std::cout << "Ertek:"<<b[i]<<std::endl;// "<<b[i]<<std::endl;" hianya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)//pontos vesszo helyett szimpla vesszo
+    for (int i = 0; i < N_ELEMENTS; i++)//pontos vesszo helyett szimpla vesszo
     {
-    {
-        atlag += b[i]// pontos vesszo hianya
+        atlag += b[i];// pontos vesszo hianya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
+    std::cout << "sajtos kifli" << std::endl;   
 }
