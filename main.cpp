e6eed93ff559b463b6e43f9c7bd39976cf200b17
nemethmiklos1996 <93788055+nemethmiklos1996@users.noreply.github.com>
2022-09-09 19:12:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,5 +23,5 @@ int main()
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
-    std::cout << "sajtos kifli" << std::endl;   
+    std::cout << "valami" << std::endl;   
 }
